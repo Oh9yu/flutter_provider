ada47884df2a2ad48764ece1da1448db230e3118
Oh9yu <kog6148@gmail.com>
2023-09-24 16:53:58 +0900

    Provider : consumer

---
+++ provider_overiview_05/android/app/src/main/kotlin/com/example/provider_overiview_05/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_overiview_05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ provider_overiview_05/lib/main.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_overiview_05/models/dog.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Dog(name: 'dog', breed: 'breed', age: 3),
+      child: MaterialApp(
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Provider'),
+        ),
+        body: Consumer<Dog>(
+          builder: (BuildContext context, Dog dog, Widget? child) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Name: ${dog.name}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  BreedAndAge()
+                ],
+              ),
+            );
+          },
+        ));
+  }
+}
+
+class BreedAndAge extends StatelessWidget {
+  const BreedAndAge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Dog>(
+      builder: (_, Dog dog, __) {
+        return Column(
+          children: [
+            Text(
+              'Breed: ${dog.breed}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Age(),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Age extends StatelessWidget {
+  const Age({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Dog>(builder: (_, Dog dog, __) {
+      return Column(
+        children: [
+          Text(
+            'Age : ${dog.age}',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(onPressed: () => dog.grow(), child: Text('grow up'))
+        ],
+      );
+    });
+  }
+}
