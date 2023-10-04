374ad17f9216805aedba55519bcca37c5a10e52c
Oh9yu <kog6148@gmail.com>
2023-10-04 22:29:07 +0900

    ADD :TODO  model, initial state

---
+++ todo_app/lib/providers/todo_filter.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/todo_model.dart';
+
+class TodoFilterState extends Equatable {
+  final Filter filter;
+
+  TodoFilterState({required this.filter});
+
+  factory TodoFilterState.initial() {
+    return TodoFilterState(filter: Filter.all);
+  }
+
+  @override
+  List<Object> get props => [filter];
+
+  TodoFilterState copyWith({
+    Filter? filter,
+  }) {
+    return TodoFilterState(
+      filter: filter ?? this.filter,
+    );
+  }
+}
+
+class TodoFilter with ChangeNotifier {
+  TodoFilterState _state = TodoFilterState.initial();
+  TodoFilterState get state => _state;
+
+  void changeFilter(Filter newFilter) {
+    _state = _state.copyWith(filter: newFilter);
+    notifyListeners();
+  }
+}
